8a99e2acbb1843c5908f019e8814cd6eba48108d
Hyuri.dev <jeffersoonpaez@gmail.com>
2025-07-18 16:00:07 -0400

    Avanzado el crud hasta leer datos y mostrarlos

--- hello_python/crud/database.py
+++ hello_python/crud/database.py
@@ -1,12 +1,16 @@
 import sqlite3 as lite3
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox 
+import os
 
-conexion = lite3.connect(r"C:\Users\Jefferson\Documents\hello python\Python\hello_python\crud\users.db")
+
+database = os.path.join(os.path.dirname(__file__), "users.db") # Ruta relativa de nuestra BD
+
+conexion = lite3.connect(database)
 
 try:
   conexion.execute(""" CREATE TABLE IF NOT EXISTS usuarios (
-    id integer auto increment, nombre text, cedula integer primary key)""")
+    id integer primary key autoincrement , nombre text, cedula integer unique)""")
   
   # print("tabla creada con exito")
 except lite3.OperationalError as e:
@@ -19,6 +23,37 @@ def create_user(app):
   conexion.commit()
   # app.entry_nombre.get(0, tk.END) añadir este reseteo de entry
   messagebox.showinfo("Solicitud", "Usuario creado con exito")
-  conexion.close()
+  mostrar_usuarios(app)
   app.entry_nombre.delete(0, tk.END)
   app.entry_identificacion.delete(0, tk.END)
+
+def search_user(app):
+  buscador = app.entry_busqueda_usuario.get()
+  cur = conexion.cursor()
+  cur.execute(f"SELECT * FROM usuarios WHERE cedula = ?", (buscador,))
+  resultado = cur.fetchone()
+  
+  if resultado:
+    nombre = resultado[1]
+    cedula = str(resultado[2])
+    texto = f"Usuario: {nombre} {cedula}"
+  else:
+    texto = "Usuario no encontrado"
+    
+  app.text_user.config(text= texto)
+  conexion.commit()
+
+
+def mostrar_usuarios (app):
+  cur = conexion.cursor()
+  cur.execute("SELECT * FROM usuarios ")
+  resultado = cur.fetchall()
+  
+  for item in app.treeview_usuarios.get_children():
+    app.treeview_usuarios.delete(item)
+  
+  #insertar usuarios 
+  
+  for fila in resultado:
+    app.treeview_usuarios.insert("", "end", values=(fila[0], fila[1], fila[2]))
+
--- hello_python/crud/main.py
+++ hello_python/crud/main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import ttkbootstrap as tkb
 from ttkbootstrap.constants import * 
 from tkinter import ttk
-from database import create_user
+from database import create_user , search_user, mostrar_usuarios
 
 
 class App :
@@ -56,26 +56,60 @@ class App :
         #Pestaña consultar usuario
         
         self.pestaña_consultar_usuario = tk.Frame(self.pestañas)
+        self.pestaña_consultar_usuario.grid_columnconfigure(0 , weight= 1)
+        self.pestaña_consultar_usuario.grid_columnconfigure(1 , weight= 1)
+        self.pestaña_consultar_usuario.grid_columnconfigure(2 , weight= 1)
+        
         self.lbl_consultar = tk.Label(self.pestaña_consultar_usuario, text= "Consultar usuario")
-        self.lbl_consultar.grid(row= 0 , column= 0, padx=(220, 0), pady= 30)
+        self.lbl_consultar.grid(row= 0 , column= 0,columnspan= 3, pady= 30, sticky="ew")
+        self.lbl_consultar.config(font=("Arial", 16, "bold"))
         
         self.title_buscar = tk.Label(self.pestaña_consultar_usuario, text= "Identificacion (Cedula, RIF, DNI)")
-        self.title_buscar.grid(row=1 , column= 0 , sticky="W" , padx= (20, 0))
+        self.title_buscar.grid(row=1 , column= 0 , sticky="W")
         
         self.entry_busqueda_usuario = tk.Entry(self.pestaña_consultar_usuario)
-        self.entry_busqueda_usuario.grid(row=1 , column=1, sticky="W", padx=(25, 0))
+        self.entry_busqueda_usuario.grid(row=1 , column=0, padx= (110, 10))
         
-        self.btn_buscar = tk.Button(self.pestaña_consultar_usuario, text="Buscar")
-        self.btn_buscar.grid(row=1 , column= 2, padx=(20))
+        self.btn_buscar = tk.Button(self.pestaña_consultar_usuario, text="Buscar", command= lambda: search_user(self))
+        self.btn_buscar.grid(row=1 , column= 0, padx= (290, 10))
         
         self.text_user = tk.Label(self.pestaña_consultar_usuario, text= f"Usuario: ")
-        self.text_user.grid(row= 2 , column= 1, sticky= "W")
+        self.text_user.grid(row= 2 , column= 0, sticky= "W", padx= 15 ,pady= (30, 0))
+        self.text_user.config(font=("Arial" , 14 , "bold"))
         
         
         self.pestañas.add(self.pestaña_consultar_usuario, text="Consulta de usuario")
-        self.pestañas.add(self.relleno3, text="Lista de usuarios")
+        
+        self.pestaña_listado_usuarios = tk.Frame (self.pestañas)
+        self.pestaña_listado_usuarios.grid_columnconfigure(0, weight= 1)
+        self.pestaña_listado_usuarios.grid_columnconfigure(1, weight= 1)
+        self.pestaña_listado_usuarios.grid_columnconfigure(2, weight= 1)
         
         
+        self.title_listado = tk.Label(self.pestaña_listado_usuarios, text="Listado de usuarios registrados")
+        self.title_listado.grid(row= 0 , column= 0, columnspan= 3 , sticky="ew", pady= 20)
+        self.title_listado.config(font=("Arial", 16 , "bold"))
+        
+        self.treeview_usuarios = ttk.Treeview(self.pestaña_listado_usuarios)
+        self.treeview_usuarios.grid(row= 1 , column= 0 , columnspan= 3, sticky="ew ")
+        
+        
+        self.treeview_usuarios["columns"] = ("1" , "2" , "3")
+        self.treeview_usuarios["show"] = "headings"
+        
+        self.treeview_usuarios.column("1", width= 30, anchor="center")
+        self.treeview_usuarios.column("2", width= 200, anchor="center")
+        self.treeview_usuarios.column("3", width= 200, anchor="center")
+        
+        
+        self.treeview_usuarios.heading("1",text= "ID")
+        self.treeview_usuarios.heading("2",text= "Usuario")
+        self.treeview_usuarios.heading("3",text= "Cedula")
+        
+        
+        self.pestañas.add(self.pestaña_listado_usuarios, text="Lista de usuarios")
+        
+        mostrar_usuarios(self)
         self.root.mainloop()
         
         
