cb09c37d16ba308b627905b1eaafafbbd8a413af
Jefferson <legacyofspirits167@gmail.com>
2025-08-24 22:26:34 -0400

    Modificado el archivo main de flet

--- flet/src/main.py
+++ flet/src/main.py
@@ -1,26 +1,21 @@
 import flet as ft
 
+class Contador :
+    def __init__(self,page):
+            self.counter = 0
+            self.txt_counter = ft.Text(f"{self.counter}", size = 20)
+            self.mensaje = ft.Text("", color = "red")
+            self.btn = ft.ElevatedButton("incrementar", on_click = self.on_click)
+            page.add(self.txt_counter,self.btn, self.mensaje)
+    
+    def on_click(self,e):
+        self.counter +=1
+        self.txt_counter.value =f"{self.counter}"
+        if self.counter > 5:
+            self.mensaje.value = "Gracias por probar la app, te amo ❤️"
+        e.page.update()
 
-def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
-
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-        counter.update()
-
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
-    page.add(
-        ft.SafeArea(
-            ft.Container(
-                counter,
-                alignment=ft.alignment.center,
-            ),
-            expand=True,
-        )
-    )
-
+def main(page):
+    Contador(page)
 
 ft.app(main)
