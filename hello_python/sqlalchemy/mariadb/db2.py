d2ad2a2ea36745569353c7bc08656160be338905
Jefferson <legacyofspirits167@gmail.com>
2025-08-26 22:50:01 -0400

    se intento conectar la bd al hosting

--- hello_python/sqlalchemy/mariadb/db2.py
+++ hello_python/sqlalchemy/mariadb/db2.py
@@ -2,9 +2,15 @@ from sqlalchemy import  create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-#Conector para mariadb
-engine = create_engine("mariadb+mariadbconnector://root:aquiles@localhost:3306/mi_tienda")
+usuario="jodomode_jpadmin"
+contraseña="soulkidd0809@"
+host = "147.135.104.21"
+db = "jodomode_example.bd"
 
+conn_string = f"mariadb+mariadbconnector://{usuario}:{contraseña}@{host}:3306/{db}"
+
+#Conector para mariadb
+engine = create_engine(conn_string, echo = True)
 Session = sessionmaker(bind= engine)
 
 session = Session()
