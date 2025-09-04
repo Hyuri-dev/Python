1a6aacc7703aeefff208f74e081521a066a1d5fe
Jefferson <legacyofspirits167@gmail.com>
2025-09-03 22:39:14 -0400

    creada la funcion de scraping de la tasa bcv

---
+++ hello_python/scraping/main.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+import warnings
+
+warnings.filterwarnings('ignore', category=requests.exceptions.RequestsWarning)
+
+r = requests.get('https://www.bcv.org.ve/', verify=False)
+
+clase = 'col-sm-6 col-xs-6 centrado'
+
+
+soup = BeautifulSoup(r.text, 'lxml')
+
+def obtener_bcv():
+    """
+    Funcion para obtener el valor del dolar bcv al dia actual haciendo scraping
+    """
+    divs_tasas = soup.find_all('div', class_= clase) #find_all para buscar todos los divs que contienen las tasas cambiarias
+    
+    tasa_bcv = None #iniciamos una variable none, esta nos servira para poder manejar la tasa una vez encontrada
+    
+    if divs_tasas:
+        print(f"Se encontraron {len(divs_tasas)} elementos <div> con la clase especificada")
+        
+        for div in divs_tasas:
+            etiqueta_strong = div.find('strong') #buscamos la etiqueta strong que contiene los valores
+            
+            if etiqueta_strong:
+                tasa_bcv = etiqueta_strong.get_text(strip=True)
+                tasa_formateada = tasa_bcv.replace("," , ".")
+                tasa = float(tasa_formateada)
+                print(f"Valor encontrado: {tasa}")
+                return tasa
+                
+            else:
+                print("Una de las etiquetas <div> no contenia la etiqueta <strong>")
+    else:
+        print(f"No se encontraron elementos <div> con la clase {clase} ")
+        
+tasa_actual = obtener_bcv()
+print(f"Tasa del dia de hoy: {tasa_actual}")
+
+
+
+
+
+
