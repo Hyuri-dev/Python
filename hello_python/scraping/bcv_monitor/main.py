88b1bc45367a81e9b398caf420e4166775a600f3
Hyuri.dev <jeffersoonpaez@gmail.com>
2025-10-16 11:17:48 -0400

    Comenzado el script para envio de tasa por ws

--- hello_python/scraping/bcv_monitor/main.py
+++ hello_python/scraping/bcv_monitor/main.py
@@ -16,8 +16,11 @@ def test_obtener_tasa():
 
 def obtener_bcv():
   with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
+    #Creamos un navegador, headless nos permite visualizar o no el navegador cuando arrancamos el script
+    browser = p.chromium.launch(headless=True)
+    #Creamos un contexto para el modo headless para asi simule la interaccion como un usuario
+    context = browser.new_context(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36")
+    page = context.new_page()
     
     page.goto("https://www.bcv.org.ve/")
     
@@ -25,7 +28,3 @@ def obtener_bcv():
     resultado = localizador.screenshot(path="C:\\Users\\Personal\\python\\Python\\hello_python\\scraping\\assets\\tasas.png")
     print(resultado)
     browser.close()
-
-if __name__ == "__main__":
-  # test_obtener_tasa()
-  obtener_bcv()
