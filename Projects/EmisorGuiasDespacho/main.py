8e6ad9ed9d140d10baf9e68ffe29f21ceb43c838
Hyuri.dev <jeffersoonpaez@gmail.com>
2025-10-24 15:36:42 -0400

    avanzado datos del conductor

--- Projects/EmisorGuiasDespacho/main.py
+++ Projects/EmisorGuiasDespacho/main.py
@@ -25,11 +25,45 @@ async def main():
         
         await page.get_by_placeholder("FACTURA(S)").fill("123")
         
-        await page.get_by_title("Código").fill("5459")
+        # Codigo del cliente
+        inputCodSica = page.get_by_title("Código")
+        await inputCodSica.fill("974949")
         
-        await page.get_by_role("button", name="Buscar").click()
+        buscarCodigo = inputCodSica.locator('..').get_by_role("button", name="Buscar")
+        await buscarCodigo.click()
+        selectorResultado = "div.d-flex.align-items-center.border-bottom"
+        await page.locator(selectorResultado).nth(0).click()
+        
+        #Cedula chofer
+        inputCedula = page.get_by_title("Cédula")
+        await inputCedula.fill("v7184360")
+        
+        buscarCedula = inputCedula.locator("..").get_by_role("button", name="Buscar")
+        await buscarCedula.click()
+        await page.locator(selectorResultado).nth(1).click()
+
+        
+        #Placa camion
+        inputPlaca = page.get_by_title("Placa")
+        await inputPlaca.fill("A28AS9G")
+        
+        buscarPlaca = inputPlaca.locator("..").get_by_role("button", name="Buscar")
+        await buscarPlaca.click()
+        await page.locator(selectorResultado).nth(2).click()
+        
+        #Producto
+        producto = "PASTAS ALIMENTICIAS LEY ORGANICA DE PRECIO JUSTO"
+        
+        await page.get_by_role("combobox").click()
         
         
+        campoBusqueda = page.locator(".select2-search__field")
+        await campoBusqueda.wait_for()
+        await campoBusqueda.type(text=producto, delay=50)
+        
+        resultado = page.get_by_role("treeitem", name=producto)
+        await resultado.wait_for()
+        await resultado.click()
         
         input("Prueba exitosa. Presiona Enter para cerrar.")
         await browser.close()
