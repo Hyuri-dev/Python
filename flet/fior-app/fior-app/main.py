e50d0b92a45dc889f489046763bc2978f3eb07d3
Hyuri.dev <jeffersoonpaez@gmail.com>
2026-04-19 22:29:12 -0400

    Reorganizar la estructura del proyecto: eliminar módulo de productos y agregar módulo de ventas con funciones para registrar y obtener resúmenes de ventas.

--- flet/fior-app/fior-app/main.py
+++ flet/fior-app/fior-app/main.py
@@ -1,10 +1,23 @@
-from src.modules.productos import add_product , get_products
+import flet as ft
 
+def main(page: ft.Page):
+    # Configuración básica de la ventana/pantalla
+    page.title = "Punto de Venta"
+    page.theme_mode = ft.ThemeMode.LIGHT # O DARK, según prefieras
+    page.padding = 20
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    # Un componente de prueba
+    mensaje = ft.Text(
+        value="¡Flet está instalado y funcionando!",
+        size=24,
+        weight=ft.FontWeight.BOLD
+    )
+
+    # Agregamos el componente a la pantalla
+    page.add(mensaje)
 
 if __name__ == "__main__":
-    # add_product("Zapato kike", 12.4)
-
-    lista = get_products()
-    for p in lista:
-        print(f"- ID: {p['id']} | {p['nombre']}, | Precio: Bs{p['precio_base']}")
-
+    # Iniciamos la app de Flet
+    ft.app(target=main)
